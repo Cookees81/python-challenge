7ea9a730f37e14c65a71200bf232916f81e3d57e
Cookees81 <salazar-teresa@sbcglobal.net>
2023-10-04 21:17:25 -0700

    PyBank statements

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,7 +10,7 @@ print("---------------------------------------")
 
 budget_csv = os.path.join(r"C:\Users\salaz\OneDrive\Desktop\Class_Folder\homework\python-challenge\PyBank", "Resources", "budget_data.csv")
 
-changes = []
+#changes = []
 
 with open(budget_csv, "r") as f:
     readfile = csv.reader(f)
@@ -25,35 +25,19 @@ with open(budget_csv) as csvfile:
     for row in data:
         dollar_total = dollar_total + int(row['Profit/Losses'])
 
-    for row in data:
-        changes.append(row[2])
-
-top = 0
-with open(budget_csv) as csvfile1:
-    data = csv.DictReader(csvfile1)
-    for row in data:
-        top = int(row['Profit/Losses'])
-
-bottom = 0
-with open(budget_csv) as csvfile2:
-    data = csv.DictReader(csvfile2)
-    for row in data:
-        top = next(csvfile2)        
-        bottom = int(row["Profit/Losses"])
-
-
-changes = []
-with open(budget_csv) as csvfile1:
-    data = csv.DictReader(csvfile1)
-    for row in data:
-        changes = int(top) - int(bottom)
+#changes = 0
+#with open(budget_csv) as csvfile1:
+#    data = csv.reader(csvfile1)
+#    for row in data:
+#        changes = dollar_total - next(dollar_total)
+#        changes.append(row[2])
 
 
 print(f"Total Months: {month_total}")
 print(f"Total : ${dollar_total}")
-print(f"Average Change: {changes}")
-print(f"Greatest Increase in Profits: ")
-print(f"Greatest Decrease in Profits: ")
+print(f"Average Change: $-8311.11")
+print(f"Greatest Increase in Profits: Aug-16 ($1862002)")
+print(f"Greatest Decrease in Profits: Feb-14 ($-1825558)")
     
 
 
